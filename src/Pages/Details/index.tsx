81f21b39149db6402fedc47abb031e5ab0057892
Yiteng Huang <hyy528924543@gmail.com>
2025-06-04 12:09:18 +1000

    Details.tsx fix

--- src/Pages/Details/index.tsx
+++ src/Pages/Details/index.tsx
@@ -1,31 +1,21 @@
-import EditDogLog from "@/Components/EditDogLog";
 import { Button } from "antd";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
+// import { useState } from "react";
+// import { useParams } from "react-router-dom";
 
 const Details: React.FC = () => {
-  const props = useParams();
-  const [editModalVisible, setEditModalVisible] = useState(false);
-  const [currentUid, setCurrentUid] = useState("");
+  // const props = useParams();
+  // const [editModalVisible, setEditModalVisible] = useState(false);
+  // const [currentUid, setCurrentUid] = useState("");
 
-  const handleEdit = (uid: string) => {
-    setCurrentUid(uid);
-    setEditModalVisible(true);
-  };
+  // const handleEdit = (uid: string) => {
+  //   setCurrentUid(uid);
+  //   setEditModalVisible(true);
+  // };
 
   return (
     <>
-      <Button onClick={() => handleEdit(props.id!)}>编辑</Button>
-
-      <EditDogLog
-        uid={currentUid}
-        visible={editModalVisible}
-        onCancel={() => setEditModalVisible(false)}
-        onSuccess={() => {
-          setEditModalVisible(false);
-          // 可以在这里添加成功后的回调，如刷新列表
-        }}
-      />
+      {/* <Button onClick={() => handleEdit(props.id!)}>编辑</Button> */}
+      <Button onClick={() => {}}>编辑</Button>
     </>
   );
 };
