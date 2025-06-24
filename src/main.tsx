3ce715c110964191d2039d9995d1f6b651fd6641
Yiteng Huang <hyy528924543@gmail.com>
2025-06-24 10:59:27 +1000

    firebase auth attempt fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/index.tsx";
-import "./firebase-config.js";
 
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/index.ts";
 import { LanguageProvider } from "./Context/languageContext.tsx";
+import "./firebase-config.js";
 
 createRoot(document.getElementById("root")!).render(
   <LanguageProvider>
