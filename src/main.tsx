571d5be63779122480b7b6ecb1d73f23a75f52c7
Yiteng Huang <hyy528924543@gmail.com>
2025-06-24 13:20:59 +1000

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import { createRoot } from "react-dom/client";
-// import { RouterProvider } from "react-router-dom";
-// import { router } from "./router/index.tsx";
+import "./firebase-config.js";
+import "./index.css";
 
-// import "./firebase-config.js";
-// import "./index.css";
-// import { Provider } from "react-redux";
-// import store from "./store/index.ts";
-// import { LanguageProvider } from "./Context/languageContext.tsx";
+import { LanguageProvider } from "./Context/languageContext.tsx";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/index.tsx";
+
+import store from "./store/index.ts";
 
 createRoot(document.getElementById("root")!).render(
-  // <LanguageProvider>
-  //   <Provider store={store}>
-  //     <RouterProvider router={router}></RouterProvider>
-  //   </Provider>
-  // </LanguageProvider>
-  <p>123</p>
+  <LanguageProvider>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
+  </LanguageProvider>
 );
